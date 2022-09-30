8c46de13a50a39818ae39012dfb8ba1a06804ee4
Sveta Martynova <svetamart95@gmail.com>
2022-09-30 20:45:21 +0300

    modified README file and project

--- project/Program.cs
+++ project/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = { "hello", "-2", "there", "hi", "hey", "hola", "good morning", "!" };
+string[] newArray = new string[array.Length];
+
+string[] FillNewArray(string[] array, string[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newArray[count] = array[i];
+            count++;
+        }
+    }
+    Array.Resize(ref newArray, count);
+    return newArray;
+}
+
+Console.WriteLine(String.Join(", ", FillNewArray(array, newArray)));
